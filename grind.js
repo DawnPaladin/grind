fbf3d3c964dbb380ebae4bf79422261643a7473d
James Harris <computersherpa@gmail.com>
2017-09-25 23:32:06 -0500

    enemy "melting" on/off

--- grind.js
+++ grind.js
@@ -32,7 +32,7 @@ function create() {
 	game.physics.p2.enable([ship, enemies], debug);
 	ship.body.setCircle(200);
 	ship.body.setCollisionGroup(shipCollisionGroup);
-	ship.body.collides([shipCollisionGroup, enemyCollisionGroup], collision);
+	ship.body.collides([shipCollisionGroup, enemyCollisionGroup]);
 	ship.body.fixedRotation = true;
 
 	cursors = game.input.keyboard.addKeys({
@@ -76,41 +76,43 @@ function render() {
 	// game.debug.text('A and D to rotate; spacebar to spawn enemies', 32, 32);
 }
 
-function collision(shipBody, impactorBody) {
-	var enemy = impactorBody.sprite;
-	impactorBody.removeCollisionGroup(enemyCollisionGroup);
-	var offset = [shipBody.x - impactorBody.x, shipBody.y - impactorBody.y];
-	var angle = game.math.angleBetween(shipBody.x, shipBody.y, impactorBody.x, impactorBody.y);
-	// game.physics.p2.createSpring(shipBody, impactorBody, 40, 30, 50);
-	enemy.constraints.push(game.physics.p2.createLockConstraint(shipBody, impactorBody, offset, angle, 1000));
-	enemy.damage = { max: 10, current: 10, inflicted: 0 };
-	var tweenTime = 500;
-	var sizeTween = game.add.tween(enemy.scale);
-	sizeTween.to( {x: 0, y: 0}, tweenTime, null, true)
-		.onComplete.add(function() {
-			while (enemy.constraints.length) {
-				game.physics.p2.removeConstraint(enemy.constraints.shift());
-			}
-			console.log(enemy.constraints);
-			enemy.kill();
-		}, this);
-	var damageTween = game.add.tween(enemy.damage);
-	damageTween.to( {current: 0 }, tweenTime, null, true);
-	damageTween.onUpdateCallback(function(param) {
-		var diff = enemy.damage.max - (enemy.damage.current + enemy.damage.inflicted);
-		shipModel.damage(diff);
-		enemy.damage.inflicted += diff;
-	});
+function collision(enemyBody, shipBody) {
+	var enemy = enemyBody.sprite;
+	enemyBody.removeCollisionGroup(enemyCollisionGroup);
+	var offset = [shipBody.x - enemyBody.x, shipBody.y - enemyBody.y];
+	var angle = game.math.angleBetween(shipBody.x, shipBody.y, enemyBody.x, enemyBody.y);
+	// game.physics.p2.createSpring(shipBody, enemyBody, 40, 30, 50);
+	enemy.constraints.push(game.physics.p2.createLockConstraint(shipBody, enemyBody, offset, angle, 1000));
+	if (enemy.melt) {
+		enemy.damage = { max: 10, current: 10, inflicted: 0 };
+		var tweenTime = 500;
+		var sizeTween = game.add.tween(enemy.scale);
+		sizeTween.to( {x: 0, y: 0}, tweenTime, null, true)
+			.onComplete.add(function() {
+				while (enemy.constraints.length) {
+					game.physics.p2.removeConstraint(enemy.constraints.shift());
+				}
+				enemy.kill();
+			}, this);
+		var damageTween = game.add.tween(enemy.damage);
+		damageTween.to( {current: 0 }, tweenTime, null, true);
+		damageTween.onUpdateCallback(function(param) {
+			var diff = enemy.damage.max - (enemy.damage.current + enemy.damage.inflicted);
+			shipModel.damage(diff);
+			enemy.damage.inflicted += diff;
+		});
+	}
 }
 
 function spawn(x, y) {
 	enemy = enemies.create(700, 200, 'enemy');
 	enemy.body.velocity.x = -100;
 	enemy.body.setCollisionGroup(enemyCollisionGroup);
-	enemy.body.collides([shipCollisionGroup, enemyCollisionGroup]);
+	enemy.body.collides([shipCollisionGroup, enemyCollisionGroup], collision);
 	enemy.body.sprite = enemy;
 	enemy.body.debug = debug;
 	enemy.anchor.x = 0.5;
 	enemy.anchor.y = 0.5;
 	enemy.constraints = [];
+	enemy.melt = false;
 }
